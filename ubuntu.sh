55a18942e8ccad7544ba40f5989ff4e0ab7ec1d1
Di Chen <chenditc@gmail.com>
2018-08-09 12:09:04 +0800

    增加docker安装

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+set -x
 
 sudo apt update
 
@@ -9,9 +11,16 @@ sudo apt install -y git zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || echo "ok"
 
 # Setup vim vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || echo "ok"
 vim +PluginInstall +qall
 
 # Copy vimrc
 cp ./.vimrc ~/.vimrc
 vim +PluginInstall +qall
+
+# Install docker aliyun
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
